70c9cc5ac68a9d3ff404a5b5ef0b7616613016fa
shaldon77 <ing.luisfernandocastillo@outlook.com>
2025-07-24 19:11:29 +0000

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+     server: {
+    host: '0.0.0.0',
+    port: 5173,
+    hmr: {
+      host: 'localhost', // o la IP de tu máquina si no estás en localhost
+    },
+  },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
